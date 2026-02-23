ad2fe5b3b38f70bc1a96343c277383b3676b7e1f
LeRoiAdib <dev@romain-laurent.fr>
2026-02-23 00:43:49 -0400

    fix: rename next.config.ts to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,30 +1,28 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
     ],
   },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(glsl|vs|fs|vert|frag)$/,
-      use: ['raw-loader'],
+      use: ["raw-loader"],
     });
     return config;
   },
   turbopack: {
     root: "/Users/rlaurent/Perso/test-portfolio-ai",
     rules: {
-      '*.glsl': {
-        loaders: ['raw-loader'],
-        as: '*.js',
+      "*.glsl": {
+        loaders: ["raw-loader"],
+        as: "*.js",
       },
-    }
-  }
+    },
+  },
 };
 
 export default nextConfig;
